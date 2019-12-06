dd0a149e71096a422effacc91c4fb7e9036a7169
Jason <jason.h.7249@gmail.com>
2019-12-06 05:36:36 -0500

    completed all the methods

--- profile-microservice/src/main/java/com/csc301/profilemicroservice/PlaylistDriverImpl.java
+++ profile-microservice/src/main/java/com/csc301/profilemicroservice/PlaylistDriverImpl.java
@@ -5,49 +5,126 @@ import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.StatementResult;
 import org.springframework.stereotype.Repository;
 import org.neo4j.driver.v1.Transaction;
+import org.neo4j.driver.v1.Values;
 
 @Repository
 public class PlaylistDriverImpl implements PlaylistDriver {
 
 	Driver driver = ProfileMicroserviceApplication.driver;
 
-	public static void InitPlaylistDb() {
-		String queryStr;
-
-		try (Session session = ProfileMicroserviceApplication.driver.session()) {
-			try (Transaction trans = session.beginTransaction()) {
-				queryStr = "CREATE CONSTRAINT ON (nPlaylist:playlist) ASSERT exists(nPlaylist.plName)";
-				trans.run(queryStr);
-				trans.success();
-			}
-			session.close();
-		}
-	}
+//	public static void InitPlaylistDb() {
+//		String queryStr;
+//
+//		try (Session session = ProfileMicroserviceApplication.driver.session()) {
+//			try (Transaction trans = session.beginTransaction()) {
+//				queryStr = "CREATE CONSTRAINT ON (nPlaylist:playlist) ASSERT exists(nPlaylist.plName)";
+//				trans.run(queryStr);
+//				trans.success();
+//			}
+//			session.close();
+//		}
+//	}
 
 	@Override
 	public DbQueryStatus likeSong(String userName, String songId) {
+		DbQueryStatus response = new DbQueryStatus("OK", DbQueryExecResult.QUERY_OK);
 		String queryStr;
-		
-		try (Session session = ProfileMicroserviceApplication.driver.session()) {
-			try (Transaction trans = session.beginTransaction()) {
-				queryStr = "CREATE CONSTRAINT ON (nPlaylist:playlist) ASSERT exists(nPlaylist.plName)";
-				trans.run(queryStr);
-				trans.success();
+		StatementResult queryResult;
+
+		if (userName == null || songId == null) {
+			response.setMessage("INVALID INPUT");
+			response.setdbQueryExecResult(DbQueryExecResult.QUERY_ERROR_GENERIC);
+		} else {
+			try (Session session = ProfileMicroserviceApplication.driver.session()) {
+				try (Transaction trans = session.beginTransaction()) {
+					queryStr = "MATCH (user:profile {userName:{userName}})-[:created]->(likedSongs:playlist)" +
+							   "CREATE (likedSongs)-[r:includes]->(songLiked:song {id:{songId}})" +
+							   "RETURN r";
+					queryResult = trans.run(queryStr, Values.parameters("userName", userName, "songId", songId));
+	
+					trans.success();
+				}
+				session.close();
 			}
-			session.close();
+
+			if (!queryResult.hasNext()) {
+				response.setMessage("NOT FOUND");
+				response.setdbQueryExecResult(DbQueryExecResult.QUERY_ERROR_NOT_FOUND);
+			}
 		}
-		return null;
+
+		return response;
 	}
 
 	@Override
 	public DbQueryStatus unlikeSong(String userName, String songId) {
-		
-		return null;
+		DbQueryStatus response = new DbQueryStatus("OK", DbQueryExecResult.QUERY_OK);
+		String queryStr;
+		StatementResult queryResult;
+
+		if (userName == null || songId == null) {
+			response.setMessage("INVALID INPUT");
+			response.setdbQueryExecResult(DbQueryExecResult.QUERY_ERROR_GENERIC);
+		} else {
+			try (Session session = ProfileMicroserviceApplication.driver.session()) {
+				try (Transaction trans = session.beginTransaction()) {
+//					"MATCH (user:profile {userName:{userName}})-[r:created]->(likedSongs:playlist), " +
+//					"(songLiked:song {id:{songId}}), (likedSongs)-[s:includes]->(songLiked)" +
+//					"RETURN s"
+					queryStr = "MATCH (user:profile {userName:{userName}})-[:created]->(:playlist)-[s:includes]->(:song {id:{songId}})" +
+							   "RETURN s";
+					queryResult = trans.run(queryStr, Values.parameters("userName", userName, "songId", songId));
+	
+					trans.success();
+				}
+				session.close();
+			}
+
+			if (!queryResult.hasNext()) {
+				response.setMessage("NOT FOUND");
+				response.setdbQueryExecResult(DbQueryExecResult.QUERY_ERROR_NOT_FOUND);
+			} else {
+				try (Session session = ProfileMicroserviceApplication.driver.session()) {
+					try (Transaction trans = session.beginTransaction()) {
+//						"MATCH (user:profile {userName:{userName}})-[r:created]->(likedSongs:playlist), " +
+//						"(songLiked:song {id:{songId}}), (likedSongs)-[s:includes]->(songLiked)" +
+//						"DELETE s"
+						queryStr = "MATCH (user:profile {userName:{userName}})-[:created]->(:playlist)-[s:includes]->(:song {id:{songId}})" +
+								   "DELETE s";
+						queryResult = trans.run(queryStr, Values.parameters("userName", userName, "songId", songId));
+	
+						trans.success();
+					}
+					session.close();
+				}
+			}
+		}
+
+		return response;
 	}
 
 	@Override
 	public DbQueryStatus deleteSongFromDb(String songId) {
-		
-		return null;
+		DbQueryStatus response = new DbQueryStatus("OK", DbQueryExecResult.QUERY_OK);
+		String queryStr;
+		StatementResult queryResult;
+
+		if (songId == null) {
+			response.setMessage("INVALID INPUT");
+			response.setdbQueryExecResult(DbQueryExecResult.QUERY_ERROR_GENERIC);
+		} else {
+			try (Session session = ProfileMicroserviceApplication.driver.session()) {
+				try (Transaction trans = session.beginTransaction()) {
+					queryStr = "MATCH (:playlist)-[r:includes]->(songLiked:song {id:{songId}})" +
+							   "DELETE r, songLiked";
+					queryResult = trans.run(queryStr, Values.parameters("songId", songId));
+	
+					trans.success();
+				}
+				session.close();
+			}
+		}
+
+		return response;
 	}
 }
--- profile-microservice/src/main/java/com/csc301/profilemicroservice/ProfileDriverImpl.java
+++ profile-microservice/src/main/java/com/csc301/profilemicroservice/ProfileDriverImpl.java
@@ -1,9 +1,7 @@
 package com.csc301.profilemicroservice;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.Record;
@@ -11,54 +9,166 @@ import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.StatementResult;
 
 import org.springframework.stereotype.Repository;
+
+//import com.csc301.songmicroservice.DbQueryExecResult;
+//import com.csc301.songmicroservice.DbQueryStatus;
+
 import org.neo4j.driver.v1.Transaction;
+import org.neo4j.driver.v1.Values;
 
 @Repository
 public class ProfileDriverImpl implements ProfileDriver {
 
 	Driver driver = ProfileMicroserviceApplication.driver;
 
-	public static void InitProfileDb() {
-		String queryStr;
-
-		try (Session session = ProfileMicroserviceApplication.driver.session()) {
-			try (Transaction trans = session.beginTransaction()) {
-				queryStr = "CREATE CONSTRAINT ON (nProfile:profile) ASSERT exists(nProfile.userName)";
-				trans.run(queryStr);
-
-				queryStr = "CREATE CONSTRAINT ON (nProfile:profile) ASSERT exists(nProfile.password)";
-				trans.run(queryStr);
-
-				queryStr = "CREATE CONSTRAINT ON (nProfile:profile) ASSERT nProfile.userName IS UNIQUE";
-				trans.run(queryStr);
-
-				trans.success();
-			}
-			session.close();
-		}
-	}
+//	public static void InitProfileDb() {
+//		String queryStr;
+//
+//		try (Session session = ProfileMicroserviceApplication.driver.session()) {
+//			try (Transaction trans = session.beginTransaction()) {
+//				queryStr = "CREATE CONSTRAINT ON (nProfile:profile) ASSERT exists(nProfile.userName)";
+//				trans.run(queryStr);
+//
+//				queryStr = "CREATE CONSTRAINT ON (nProfile:profile) ASSERT exists(nProfile.password)";
+//				trans.run(queryStr);
+//
+//				queryStr = "CREATE CONSTRAINT ON (nProfile:profile) ASSERT nProfile.userName IS UNIQUE";
+//				trans.run(queryStr);
+//
+//				trans.success();
+//			}
+//			session.close();
+//		}
+//	}
 	
-	@Override
-	public DbQueryStatus createUserProfile(String userName, String fullName, String password) {
-		
-		return null;
-	}
+//	@Override
+//	public DbQueryStatus createUserProfile(String userName, String fullName, String password) {
+//		String queryStr;
+//
+//		try (Session session = ProfileMicroserviceApplication.driver.session()) {
+//			try (Transaction trans = session.beginTransaction()) {
+//				queryStr = "CREATE CONSTRAINT ON (nProfile:profile) ASSERT exists(nProfile.userName)";
+//				trans.run(queryStr);
+//
+//				queryStr = "CREATE CONSTRAINT ON (nProfile:profile) ASSERT exists(nProfile.password)";
+//				trans.run(queryStr);
+//
+//				queryStr = "CREATE CONSTRAINT ON (nProfile:profile) ASSERT nProfile.userName IS UNIQUE";
+//				trans.run(queryStr);
+//
+//				trans.success();
+//			}
+//			session.close();
+//		}
+//		return null;
+//	}
 
 	@Override
 	public DbQueryStatus followFriend(String userName, String frndUserName) {
-		
-		return null;
+		DbQueryStatus response = new DbQueryStatus("OK", DbQueryExecResult.QUERY_OK);
+		String queryStr;
+		StatementResult queryResult;
+
+		if (userName == null || frndUserName == null) {
+			response.setMessage("INVALID INPUT");
+			response.setdbQueryExecResult(DbQueryExecResult.QUERY_ERROR_GENERIC);
+		} else {
+			try (Session session = ProfileMicroserviceApplication.driver.session()) {
+				try (Transaction trans = session.beginTransaction()) {
+					queryStr = "MATCH (user:profile {userName:{userName}}), (friend:profile {userName:{friendName}})" +
+							   "CREATE (user)-[r:follows]->(friend)" +
+							   "RETURN r";
+					queryResult = trans.run(queryStr, Values.parameters("userName", userName, "friendName", frndUserName));
+	
+					trans.success();
+				}
+				session.close();
+			}
+	
+			if (!queryResult.hasNext()) {
+				response.setMessage("NOT FOUND");
+				response.setdbQueryExecResult(DbQueryExecResult.QUERY_ERROR_NOT_FOUND);
+			}
+		}
+
+		return response;
 	}
 
 	@Override
 	public DbQueryStatus unfollowFriend(String userName, String frndUserName) {
-		
-		return null;
+		DbQueryStatus response = new DbQueryStatus("OK", DbQueryExecResult.QUERY_OK);
+		String queryStr;
+		StatementResult queryResult;
+
+		if (userName == null || frndUserName == null) {
+			response.setMessage("INVALID INPUT");
+			response.setdbQueryExecResult(DbQueryExecResult.QUERY_ERROR_GENERIC);
+		} else {
+			try (Session session = ProfileMicroserviceApplication.driver.session()) {
+				try (Transaction trans = session.beginTransaction()) {
+					queryStr = "MATCH (user:profile {userName:{userName}})-[r:follows]->(friend:profile {userName:{friendName}})" +
+							   "RETURN r";
+					queryResult = trans.run(queryStr, Values.parameters("userName", userName, "friendName", frndUserName));
+	
+					trans.success();
+				}
+				session.close();
+			}
+	
+			if (!queryResult.hasNext()) {
+				response.setMessage("NOT FOUND");
+				response.setdbQueryExecResult(DbQueryExecResult.QUERY_ERROR_NOT_FOUND);
+			} else {
+				try (Session session = ProfileMicroserviceApplication.driver.session()) {
+					try (Transaction trans = session.beginTransaction()) {
+						queryStr = "MATCH (user:profile {userName:{userName}})-[r:follows]->(friend:profile {userName:{friendName}})" +
+								   "DELETE r";
+						queryResult = trans.run(queryStr, Values.parameters("userName", userName, "friendName", frndUserName));
+	
+						trans.success();
+					}
+					session.close();
+				}
+			}
+		}
+
+		return response;
 	}
 
 	@Override
 	public DbQueryStatus getAllSongFriendsLike(String userName) {
-			
-		return null;
+		DbQueryStatus response = new DbQueryStatus("OK", DbQueryExecResult.QUERY_OK);
+		String queryStr;
+		StatementResult queryResult;
+
+		if (userName == null) {
+			response.setMessage("INVALID INPUT");
+			response.setdbQueryExecResult(DbQueryExecResult.QUERY_ERROR_GENERIC);
+		} else {
+			try (Session session = ProfileMicroserviceApplication.driver.session()) {
+				try (Transaction trans = session.beginTransaction()) {
+					// get 
+					queryStr = "MATCH (user:profile {userName:{userName}})-[:follows]->(:profile)-[:created]->(:playlist)-[:includes]->(friendSong:song)" +
+							   "RETURN friendSong";
+					queryResult = trans.run(queryStr, Values.parameters("userName", userName));
+	
+					trans.success();
+				}
+				session.close();
+			}
+
+			if (!queryResult.hasNext()) {
+				response.setMessage("NOT FOUND");
+				response.setdbQueryExecResult(DbQueryExecResult.QUERY_ERROR_NOT_FOUND);
+			} else {
+				List<Object> queryItemList = new ArrayList<Object>();
+				while (queryResult.hasNext()) {
+					queryItemList.add(queryResult.next().asMap());
+				}
+				response.setData(queryItemList);
+			}
+		}
+
+		return response;
 	}
 }
